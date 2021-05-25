546b8246056eaef9a774435819ef68f465b40fbe
Azamatjon Abdusalomov <jaxongirabdusalomov0102@gmail.com>
2021-05-25 01:09:27 -0700

    e-commerce-app

--- src/components/checkoutForm/PaymentForm.js
+++ src/components/checkoutForm/PaymentForm.js
@@ -1,15 +1,37 @@
 import React from "react";
-import {Typography} from "@material-ui/core";
-import {Elements} from "@stripe/react-stripe-js";
-import {loadStrip} from "@stripe/stripe-js";
+import {Button, Divider, Typography} from "@material-ui/core";
+import {CardElement, Elements, ElementsConsumer} from "@stripe/react-stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
 
 import Review from "./Review";
 
-const PaymentForm = ({shippingData}) => {
+const stripePromise = loadStripe('...');
+
+const PaymentForm = ({checkoutToken, backStep}) => {
     return (
         <>
 
-            <Review/>
+            <Review checkoutToken={checkoutToken}/>
+            <Divider/>
+            <Typography variant="h6" gutterBottom style={{margin: "20px 0"}}>Payment Method</Typography>
+            <Elements stripe={stripePromise}>
+                <ElementsConsumer>
+                    {
+                        ({elements, stripe}) => (
+                            <form>
+                                <CardElement/>
+                                <br/><br/>
+                                <div style={{display: "flex", justifyContent: "space-between"}}>
+                                    <Button variant="outlined" onClick={backStep}>Back</Button>
+                                    <Button variant="contained" type="submit" disabled={!stripe} color="primary">
+                                        Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+                                    </Button>
+                                </div>
+                            </form>
+                        )
+                    }
+                </ElementsConsumer>
+            </Elements>
 
         </>
     )
